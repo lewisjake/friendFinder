abc8ccc6968b1d25b9ea7297dd0896596083d66a
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-23 00:32:32 -0700

    added routes for the html pages.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// include packages
+var path = require("path");
+
+// export html route
+module.exports = function(app) {
+
+    // home page
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/home.hhtml'));
+    });
+
+    // survey page
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/survey.html');
+    });
+};
