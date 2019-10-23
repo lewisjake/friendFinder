b0baf43009eb8ca1955532fb5874a5241acb3232
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-22 17:37:09 -0700

    created inital array of friends.

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+// create array to hold friend data
+var friendsArray = [
+    {
+        name: "Rick Sanchez",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Morty Smith",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Summer Smith",
+        photo: "https://en.wikipedia.org/wiki/List_of_Rick_and_Morty_characters#Summer_Smith",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Jerry Smith",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest?cb=20160923151111",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Beth Smith",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/5/58/Beth_Smith.png/revision/latest?cb=20151204220729",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Bird Person",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/b/b1/Birdperson.png/revision/latest?cb=20140415133633",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Squanchy",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/1/16/Squanchy_.png/revision/latest?cb=20160830140218",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Mr. Meeseeks",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/6/6c/MeeseeksHQ.png/revision/latest?cb=20150930232412",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Noob-Noob",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/c/c8/Noob_Noob.png/revision/latest?cb=20180614022930",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+    {
+        name: "Evil Morty",
+        photo: "https://vignette.wikia.nocookie.net/rickandmorty/images/8/80/S3e7_evil_morty.png/revision/latest?cb=20180504132753",
+        scores: [
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+            '',
+        ]
+    },
+]
