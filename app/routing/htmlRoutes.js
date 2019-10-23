b550aa2940f1bcc9144c4d05b351a1e7c5f37717
Jacob Lewis <jacoblewis@Jacobs-MacBook-Pro.local>
2019-10-23 11:35:02 -0700

    updated htmlRoute.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,6 @@ module.exports = function(app) {
 
     // survey page
     app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/survey.html');
+        res.sendFile(path.join(__dirname, '../public/survey.html'));
     });
 };
